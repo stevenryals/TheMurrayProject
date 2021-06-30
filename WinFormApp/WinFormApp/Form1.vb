f719bed3185c146a020ca2a9873fe958e6862871
guitarsr@bellsouth.net <guitarsr@bellsouth.net>
2021-06-30 16:25:18 -0500

    transform text - backwards test

--- WinFormApp/WinFormApp/Form1.vb
+++ WinFormApp/WinFormApp/Form1.vb
@@ -1,13 +1,26 @@
 ﻿Public Class Form1
 
     Public Sub New()
-
+        InitializeComponent()
     End Sub
 
     Private Sub searchButton_Click(sender As Object, e As EventArgs) Handles searchButton.Click
-        'get the value from the text box
-        'call a method named "search" and store the respone
-        'call a method named "parse" and store the repsonse
-        'display the results in the richTextBox
+        Dim response As String = transform(productTextbox.Text)
+        resultTextBox.Text = response
     End Sub
+
+    Private Function transform(ByVal str As String)
+        Dim ss As String = str.Substring(str.Length - 1, 1)
+
+        Dim backwards As String = ""
+        Dim i As Integer = -1
+        While i > ((str.Length + 1) * -1)
+            backwards = backwards + str.Substring(str.Length + i, 1)
+            i = i - 1
+        End While
+
+        Return backwards
+    End Function
 End Class
+
+
