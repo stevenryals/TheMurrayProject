5aa9c3c23e548ef366f8655869b401417fae6882
Steven Ryals <guitarsr@bellsouth.net>
2021-06-30 09:29:21 -0500

    Initial Commit

---
+++ WinFormApp/WinFormApp/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.productTextbox = New System.Windows.Forms.TextBox()
+        Me.searchButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'productTextbox
+        '
+        Me.productTextbox.Location = New System.Drawing.Point(174, 35)
+        Me.productTextbox.Name = "productTextbox"
+        Me.productTextbox.Size = New System.Drawing.Size(150, 20)
+        Me.productTextbox.TabIndex = 0
+        '
+        'searchButton
+        '
+        Me.searchButton.Location = New System.Drawing.Point(330, 33)
+        Me.searchButton.Name = "searchButton"
+        Me.searchButton.Size = New System.Drawing.Size(75, 23)
+        Me.searchButton.TabIndex = 1
+        Me.searchButton.Text = "Search"
+        Me.searchButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(75, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Product to Search"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(3, 62)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(564, 368)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(610, 451)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.searchButton)
+        Me.Controls.Add(Me.productTextbox)
+        Me.Name = "Form1"
+        Me.Text = "Product Lister Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents productTextbox As TextBox
+    Friend WithEvents searchButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ WinFormApp/WinFormApp/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Public Sub New()
+
+    End Sub
+
+    Private Sub searchButton_Click(sender As Object, e As EventArgs) Handles searchButton.Click
+        'get the value from the text box
+        'call a method named "search" and store the respone
+        'call a method named "parse" and store the repsonse
+        'display the results in the richTextBox
+    End Sub
+End Class
