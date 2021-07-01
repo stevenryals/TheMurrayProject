b2895e0b926111fceba62656d7fb0d7200efcbf2
guitarsr@bellsouth.net <guitarsr@bellsouth.net>
2021-07-01 09:38:07 -0500

    Added numbers to add together to form.  ready for someone to do the code on it.

--- WinFormApp/WinFormApp/Form1.Designer.vb
+++ WinFormApp/WinFormApp/Form1.Designer.vb
@@ -23,52 +23,114 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.productTextbox = New System.Windows.Forms.TextBox()
-        Me.searchButton = New System.Windows.Forms.Button()
+        Me.backwardsButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.firstNumberToAddTextBox = New System.Windows.Forms.TextBox()
+        Me.secondNumberToAddTextBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'productTextbox
         '
-        Me.productTextbox.Location = New System.Drawing.Point(174, 35)
+        Me.productTextbox.Location = New System.Drawing.Point(128, 21)
         Me.productTextbox.Name = "productTextbox"
-        Me.productTextbox.Size = New System.Drawing.Size(150, 20)
+        Me.productTextbox.Size = New System.Drawing.Size(111, 20)
         Me.productTextbox.TabIndex = 0
         '
-        'searchButton
+        'backwardsButton
         '
-        Me.searchButton.Location = New System.Drawing.Point(330, 33)
-        Me.searchButton.Name = "searchButton"
-        Me.searchButton.Size = New System.Drawing.Size(75, 23)
-        Me.searchButton.TabIndex = 1
-        Me.searchButton.Text = "Search"
-        Me.searchButton.UseVisualStyleBackColor = True
+        Me.backwardsButton.Location = New System.Drawing.Point(245, 19)
+        Me.backwardsButton.Name = "backwardsButton"
+        Me.backwardsButton.Size = New System.Drawing.Size(134, 23)
+        Me.backwardsButton.TabIndex = 1
+        Me.backwardsButton.Text = "Make it Backwards"
+        Me.backwardsButton.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(75, 38)
+        Me.Label1.Location = New System.Drawing.Point(29, 24)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.Size = New System.Drawing.Size(89, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Product to Search"
+        Me.Label1.Text = "String to Reverse"
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(3, 62)
+        Me.RichTextBox1.Location = New System.Drawing.Point(385, 21)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(564, 368)
+        Me.RichTextBox1.Size = New System.Drawing.Size(198, 58)
         Me.RichTextBox1.TabIndex = 3
         Me.RichTextBox1.Text = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(428, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Results Go Here"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(25, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "numbers to add"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(241, 86)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Add the numbers"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'firstNumberToAddTextBox
+        '
+        Me.firstNumberToAddTextBox.Location = New System.Drawing.Point(124, 88)
+        Me.firstNumberToAddTextBox.Name = "firstNumberToAddTextBox"
+        Me.firstNumberToAddTextBox.Size = New System.Drawing.Size(26, 20)
+        Me.firstNumberToAddTextBox.TabIndex = 5
+        '
+        'secondNumberToAddTextBox
+        '
+        Me.secondNumberToAddTextBox.Location = New System.Drawing.Point(180, 88)
+        Me.secondNumberToAddTextBox.Name = "secondNumberToAddTextBox"
+        Me.secondNumberToAddTextBox.Size = New System.Drawing.Size(26, 20)
+        Me.secondNumberToAddTextBox.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(156, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "+"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(610, 451)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.secondNumberToAddTextBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.firstNumberToAddTextBox)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.searchButton)
+        Me.Controls.Add(Me.backwardsButton)
         Me.Controls.Add(Me.productTextbox)
         Me.Name = "Form1"
         Me.Text = "Product Lister Test"
@@ -78,7 +140,13 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents productTextbox As TextBox
-    Friend WithEvents searchButton As Button
+    Friend WithEvents backwardsButton As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents firstNumberToAddTextBox As TextBox
+    Friend WithEvents secondNumberToAddTextBox As TextBox
+    Friend WithEvents Label4 As Label
 End Class
--- WinFormApp/WinFormApp/Form1.vb
+++ WinFormApp/WinFormApp/Form1.vb
@@ -4,12 +4,12 @@
         InitializeComponent()
     End Sub
 
-    Private Sub searchButton_Click(sender As Object, e As EventArgs) Handles searchButton.Click
-        Dim response As String = transform(productTextbox.Text)
-        resultTextBox.Text = response
+    Public Sub backwardsButton_Click(sender As Object, e As EventArgs) Handles backwardsButton.Click
+        Dim response As String = makeItBackwards(productTextbox.Text)
+        RichTextBox1.Text = response
     End Sub
 
-    Private Function transform(ByVal str As String)
+    Private Function makeItBackwards(ByVal str As String)
         Dim ss As String = str.Substring(str.Length - 1, 1)
 
         Dim backwards As String = ""
@@ -19,8 +19,10 @@
             i = i - 1
         End While
 
+
         Return backwards
     End Function
+
 End Class
 
 
