4eb087e0b6bb653449b09bdf63cb5c60f704f35e
Matthew Murray <mattm2411@outlook.com>
2021-09-08 16:42:45 -0500

    Commit before reformatting Class for D&D Player

--- WinFormApp/WinFormApp/Class1.vb
+++ WinFormApp/WinFormApp/Class1.vb
@@ -1,4 +1,15 @@
-﻿
+﻿Public Class Character
+
+    Public Name As String
+    Public Attributes As String
+    Public Strength As Integer
+    Public Dexterity As Integer
+    Public Constitution As Integer
+    Public Intelligence As Integer
+    Public Wisdom As Integer
+    Public Charisma As Integer
+
+End Class
 
 
 
